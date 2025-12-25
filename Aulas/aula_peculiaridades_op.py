a619a39690f2f5d247ca7249aef08f350715168a
Lucca Serafim <lucca.serafim@gmail.com>
2025-12-25 18:17:53 -0300

    Aula falando sobre concatenação e repetição usando os operadores + e *

---
+++ Aulas/aula_peculiaridades_op.py
@@ -0,0 +1,11 @@
+# Aula falando algumas peculiaridades dos operadores + e do *
+# A concatenação usa o + e a repetição usa o *
+# Na concatenação tomar cuidado para não misturar as tipagens, ex: int + str, pois isso da erro no python, qualquer coisa so fazer a coerção dele
+# Na repetição tem que usar o int e uma string
+
+
+concatenacao = "C" + 'R' + "L" + 'F'
+print(concatenacao)
+
+k_sete_vezes = 7 * 'K'
+print(k_sete_vezes)
