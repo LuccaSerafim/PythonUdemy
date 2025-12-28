a04b53648a08246243be33eb845382f40853fbe3
Lucca Serafim <lucca.serafim@gmail.com>
2025-12-28 15:55:53 -0300

    Aula sobre os principais operadores aritméticos

---
+++ Aulas/ModuloBasico/aula_operadores_arit.py
@@ -0,0 +1,37 @@
+"""
+Aula sobre os principais operadores aritmeticos: soma, subtraçao, multiplicação, divisao onde o resultado é um float,
+divisao inteira,exponencial e modulo.
+
+OBS: Caso eu faça um conta de divisao inteira com numero flutuante, o resultado vai cortar as casas decimais.
+OBS2: Modulo vai retornar o resto de uma divisao. Ex: 25 % 5
+
+Caracteres de cada operador:
+Soma é +
+Subtraçao é -
+Multiplicação é *
+Divisão é /
+Divisão Inteira é //
+Exponencial é **
+Módulo é %
+
+"""
+
+soma = 70 + 30
+subtracao = 30 - 15
+multipicacao = 3 * 3
+divisao = 27 / 4
+div_inteira = 27 // 4
+exponencial = 2 ** 5
+modulo = 37 % 6
+
+print("A soma é:", soma)
+print('A subtração é:', subtracao)
+print("A multiplicação é:", multipicacao)
+print("A divisão é:", divisao)
+print("A divisão inteira é:", div_inteira)
+print("O exponencial é:", exponencial)
+print("O módulo é:", modulo)
+
+
+
+
