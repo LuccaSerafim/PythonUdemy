733cd544672edfb54d71beeab3ae877cbde2df90
Lucca Serafim <lucca.serafim@gmail.com>
2025-12-28 16:11:01 -0300

    Removeu arquivos duplicados da pasta Aulas

---
+++ teste.py
@@ -0,0 +1,8 @@
+impar = int(input("Digite um Número: "))
+par = int(input("Digite outro Número: "))
+
+if (impar % 3) == 1:
+    print("Este e um número impar")
+
+elif (par % 2) == 0:
+    print("Este e um número par")
