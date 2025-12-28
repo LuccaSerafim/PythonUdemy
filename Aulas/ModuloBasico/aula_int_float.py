5248dbcd7b9c55a089c5c7a3abd540967a8fef60
Lucca Serafim <lucca.serafim@gmail.com>
2025-12-28 15:54:10 -0300

    Aula falando sobre os tipos de dados int e float

---
+++ Aulas/ModuloBasico/aula_int_float.py
@@ -0,0 +1,14 @@
+"""
+# Int = Inteiro(tanto positivo quanto negativo)
+# Float = Flutuante(Usado para separar as casas decimais de um numero, tanto positivo quanto negativo)
+# Exemplo 10.15 (OBS: Para separar as casa decimais usar o . pois a , e para separar os argumentos)
+# para comentar linhas usar o atalho ctrl e ; 
+# função type me mostra qual o tipo do dado nessa aula
+"""
+print(10)
+print(-10)
+print(10.15)
+print(10.17, -15.24)
+
+print(type("Nba"))
+print(type("Nba"), type(1.1), type(-15.49))
