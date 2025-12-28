5c473de78f96ca6a113258e3016e3787ea799917
Lucca Serafim <lucca.serafim@gmail.com>
2025-12-28 16:13:25 -0300

    Apagando teste

--- teste.py
+++
@@ -1,8 +0,0 @@
-impar = int(input("Digite um Número: "))
-par = int(input("Digite outro Número: "))
-
-if (impar % 3) == 1:
-    print("Este e um número impar")
-
-elif (par % 2) == 0:
-    print("Este e um número par")
