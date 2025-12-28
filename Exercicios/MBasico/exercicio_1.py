144bc89f14a661040a69cb23d1dd7199519224d6
Lucca Serafim <lucca.serafim@gmail.com>
2025-12-28 16:16:05 -0300

    Exercício 1 onde temos os tipos de dados e variáveis

---
+++ Exercicios/MBasico/exercicio_1.py
@@ -0,0 +1,15 @@
+# Exercício da Aula 25, onde temos que definir algumas variaveis de todos os tipos de dados visto ate agora e imprimir no terminal com os prints.
+
+nome = "Cleiton"
+sobrenome = 'Rasta'
+idade = 20
+ano_nascimento = 2005
+maior_de_idade = idade >= 18
+altura_metros = 1.82
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de Nascimento:", ano_nascimento)
+print("É maior de idade?", maior_de_idade)
+print("Altura em Metros:", altura_metros)
