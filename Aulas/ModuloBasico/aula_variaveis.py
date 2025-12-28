53768d804a4d597acd075f77372d7025814f99e9
Lucca Serafim <lucca.serafim@gmail.com>
2025-12-28 16:00:11 -0300

    Aula falando sobre variaveis e como definir elas

---
+++ Aulas/ModuloBasico/aula_variaveis.py
@@ -0,0 +1,17 @@
+"""
+Aula de introdução as variaveis
+Recomendavel usar letras minusculas para as variaveis, mas podendo tbm numeros e o _ tbm apos a letra minuscula
+variaveis sao usadas para salvar algo na "memoria" do computador
+O sinal de = é usado para atribuir um valor a um nome(variavel). Caso queira dizer que algo e igual a outro usa-se ==
+Variaveis NAO SAO USADAS PARA ABREVIAR CODIGOS. ELAS SAO USADAS PARA TORNAR O CODIGO MAIS LEGIVEL E NAO REPETIR EM LUGARES
+QUE VOCE VAI USAR A MESMA COISA
+Podemos fazer boas praticas para evitar a repetiçao de nome, exemplo da outra aula: 
+print(int('1') + 3, type(int('1')))
+"""
+
+
+data_nascimento = 21.07
+nome = "Steph Curry"
+exp_int = int('1')
+print(data_nascimento, nome,sep='-')
+print(exp_int + 3, type(exp_int))
