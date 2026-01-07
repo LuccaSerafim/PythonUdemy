6022d7033104da0c8b6dcd5ea0bc01aec9faee6c
Lucca Serafim <lucca.serafim@gmail.com>
2026-01-07 17:41:54 -0300

    Aula falando while dentro de outro while

---
+++ Aulas/ModuloBasico/aula_lacos_internos_while.py
@@ -0,0 +1,16 @@
+# Aula de while dentro de while
+
+# Código do professor, estou sem criatividade.
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    linha += 1
+
+
+print('Acabou')
