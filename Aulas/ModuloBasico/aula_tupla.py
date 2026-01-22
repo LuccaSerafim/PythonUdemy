8541c183f94620a9c08e8981342a24c1a842fabd
Lucca Serafim <lucca.serafim@gmail.com>
2026-01-22 16:34:46 -0300

    Aula sobre tuplas

---
+++ Aulas/ModuloBasico/aula_tupla.py
@@ -0,0 +1,14 @@
+"""
+Aula falando sobre Tuple (tupla):
+É um tipo de Lista so que IMUTAVEL
+Uma tupla é mais eficiente que uma lista, porem a lista a gente pode usar metodos e mexer nela.
+
+Para fazer uma tupla, um dos jeitos é apenas tirar os [] da lista ou substituir por () ou entao fazer a coerção
+com tuple()
+EXLIsta: nome = ["João", "Antonio", "Jesus"]
+EXTupla: nome = "João", "Antonio", "Jesus" | nome = ("João", "Antonio", "Jesus") | nome = tuple(nome)
+
+
+
+
+"""
